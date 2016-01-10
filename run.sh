ea362b7473265c4c3b07a0101e89e186b826d410
Kyle McDonald <kyle@kylemcdonald.net>
2016-01-10 17:03:07 +0900

    remove container on exit

--- run.sh
+++ run.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
-# Start Quickstart Terminal if it's not already started.
-# if [ ! -z "$DOCKER_MACHINE" ]; then
-#	bash --login '/Applications/Docker/Docker Quickstart Terminal.app/Contents/Resources/Scripts/start.sh'
-# fi
+# set up docker
+# eval "$(docker-machine env --shell=bash default)"
 
 jupyterport="8888"
 ip=`docker-machine ip default`
 
 openavailable(){
 	# echo "Opening http://$1:$2/ once it is available..."
+	# wait for nc to connect to the port
+	# todo: windows?
 	until nc -vz "$1" "$2" &>/dev/null; do
 		sleep 1
 	done
+	# todo: windows and linux
 	open "http://$1:$2/"
 }
 
@@ -20,13 +21,15 @@ openavailable $ip $jupyterport &
 
 dir=`pwd`
 docker run -ti \
-	-p "$jupyterport:$jupyterport" \
+	--rm=true \
+	--name="ml-notebook" \
+	--publish="$jupyterport:$jupyterport" \
 	--env "HOST_IP=$ip" \
-	-w "/root/shared" \
+	--workdir="/root/shared" \
 	--volume="$dir/shared:/root/shared" \
 	kylemcdonald/ml-notebook \
 	/bin/bash -c " \
 		sudo ln /dev/null /dev/raw1394 ; \
 		jupyter notebook --ip='*' --no-browser > jupyter.log 2>&1 & \
-		echo 'Jupyter is at http://$ip:$jupyterport/ and writing to jupyter.log'
+		echo 'Jupyter is at http://$ip:$jupyterport/ and writing to jupyter.log' ; \
 		bash"
