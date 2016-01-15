d1cd1ca86544638d18f6bf2aa1120072b6398c8b
Kyle McDonald <kyle@kylemcdonald.net>
2016-01-16 02:22:28 +0900

    packaging script correction

--- package.sh
+++ package.sh
@@ -16,6 +16,6 @@ rm shared/jupyter.log
 HASH=`git rev-parse HEAD | cut -c-8`
 ZIPFILE="../ml-notebook-$HASH.zip"
 
-zip -q -r ZIPFILE ./
+zip -q -r "$ZIPFILE" ./
 
 echo "Package is ready: $ZIPFILE"
